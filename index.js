bb416f6d13331dbb7e0ac0866b276b61df654e09
esraamagddi <esraamagdy20133@gmail.com>
2024-06-17 20:21:34 +0300

    feat:add DB connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-
 import { port } from './config.env.js';
 import dotenv from 'dotenv'
+import connectDB from "./databases/dbConnection.js";
 
 dotenv.config();
 
@@ -10,3 +10,5 @@ const app = express();
 const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
+  
+connectDB();  
