b4d1454d83f0ab0e56f7bcf859c7f828ce33dec6
esraamagddi <esraamagdy20133@gmail.com>
2024-06-17 23:35:36 +0300

    feat:add rest of Project CRUD

--- src/modules/project/project.controllers.js
+++ src/modules/project/project.controllers.js
@@ -9,3 +9,53 @@ export  const createProject = asyncHandler(async (req, res) => {
   });
 
 
+  export const getAllProjects = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10 } = req.query; 
+    const skip = (page - 1) * limit;
+  
+    const projects = await Project.find()
+                                  .skip(skip)
+                                  .limit(limit)
+                                  .sort({ name: 1 });
+    const totalProjects = await Project.countDocuments();
+  
+    res.json({
+      projects,
+      totalPages: Math.ceil(totalProjects / limit),
+      currentPage: page
+    });
+  });
+
+  export const getProjectById = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
+    
+    if (!project) {
+      throw new CustomError('Project not found', 404);
+    }
+  
+    res.json(project);
+  });
+
+  export const updateProject = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const updatedFields = req.body;
+    const updatedProject = await Project.findByIdAndUpdate(projectId, updatedFields, { new: true });
+  
+    if (!updatedProject) {
+      throw new CustomError('Project not found', 404);
+    }
+  
+    res.json(updatedProject);
+  });
+  
+  export const deleteProject = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const deletedProject = await Project.findByIdAndDelete(projectId);
+  
+    if (!deletedProject) {
+      throw new CustomError('Project not found', 404);
+    }
+  
+    res.json({ message: 'Project deleted successfully' });
+  });
--- src/modules/project/project.routes.js
+++ src/modules/project/project.routes.js
@@ -3,6 +3,9 @@ import * as projectController from './project.controllers.js';
 
 const router = express.Router();
 router.post('/', projectController.createProject);
-
+router.get('/', projectController.getAllProjects);
+router.get('/:projectId', projectController.getProjectById);
+router.put('/:projectId', projectController.updateProject);
+router.delete('/:projectId', projectController.deleteProject);
 
 export default router;
