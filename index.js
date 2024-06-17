b8be4da72a2600299a92792efba0867d4029dc1a
esraamagddi <esraamagdy20133@gmail.com>
2024-06-17 20:28:53 +0300

    feat:add cors configrations

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ import express from 'express';
 import { port } from './config.env.js';
 import dotenv from 'dotenv'
 import connectDB from "./databases/dbConnection.js";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
-  
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: process.env.CORS_METHODS.split(','), 
+  allowedHeaders: process.env.CORS_ALLOWED_HEADERS.split(','), 
+  credentials: process.env.CORS_CREDENTIALS === 'true'
+}));  
 connectDB();  
